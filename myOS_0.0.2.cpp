4e86f849a6d3fc6e6e9672e29bd5e91e0a44df0a
linuxsoid <r4351546@gmail.com>
2026-08-04 22:25:47 +0300

    Add files via upload

--- myOS_0.0.2.cpp
+++ myOS_0.0.2.cpp
@@ -2,10 +2,73 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <fstream>
 
 
 using namespace std;
 
+struct foldersesc{
+string name;
+foldersesc* parent = nullptr;
+map<string , foldersesc*> children;
+};
+foldersesc root{"|" , nullptr};
+foldersesc* current = &root;
+
+
+void vect(vector<string> predmets) {
+for(auto p : predmets) {
+cout << "-- " << p << endl;
+}
+}
+void cels(const string& filename) {
+
+vector<string> lines_vect1;
+cout << "        ===CELS TEXT EDITOR===\n\n";
+vector<string> fols;
+string curentline;
+while(true) {
+cout << ":>>";
+getline(cin, curentline);
+if(curentline == ":wq") {
+ofstream file(filename);
+for(const auto& x : lines_vect1 ) {
+file << x << endl;
+}
+file.close();
+}
+else {
+lines_vect1.push_back(curentline);
+}
+}
+}
+void calulator() {
+double num1 , num2;
+char symb;
+    cin >> num1 >> symb >> num2;
+    if(symb == '+') {
+        cout << "result for add: " << num1 + num2 << endl;
+
+    }
+
+    else if(symb == '-') {
+        cout << "result for minus: " << num1 - num2 << endl;
+
+    }
+
+    else if(symb == '*') {\
+        cout << "result for multiply: " << num1 * num2 << endl;
+
+    }
+
+    else if(symb == '/') {
+        cout << "result for divide: " << num1 / num2 << endl;
+        if(num1 == 0 || num2 == 0) {
+            cout << "[ERROR]: divide not work with '0'" << endl;
+        }
+    } 
+}
 void nio() {
     
     #define RESET   "\033[0m"
@@ -17,13 +80,13 @@ void nio() {
 
     std::cout << CYAN << "  ████████  ████████ " << GREEN << "  root" << RESET << "@" << GREEN << "tfos\n";
     std::cout << CYAN << "     ██     ██       " << RESET << "  -------------\n";
-    std::cout << CYAN << "     ██     ██████   " << YELLOW << "  OS: " << RESET << "T-FOS v0.1 (Terminal-Friendly)\n";
-    std::cout << CYAN << "     ██     ██       " << YELLOW << "  Shell: " << RESET << "TH-Shell (Custom Engine)\n";
+    std::cout << CYAN << "     ██     ██████   " << YELLOW << "  OS: " << RESET << "T-FOS v0.0.3 (Terminal-Friendly)\n";
+    std::cout << CYAN << "     ██     ██       " << YELLOW << "  Shell: " << RESET << "TH-Shell\n";
     std::cout << CYAN << "     ██     ██       " << YELLOW << "  Host: " << RESET << "emulator\n";
-    std::cout << CYAN << "                     " << YELLOW << "  Architecture: " << RESET << "C++ Bare Logic\n\n";
+    std::cout << CYAN << "                     " << YELLOW << "  Architecture: " << RESET << "C++\n\n";
     
     
-    std::cout << "  " << "███ " << GREEN << "███ " << YELLOW << "███ " << BLUE << "███ " << MAGENTA << "███ " << CYAN << "███" << RESET << "\n\n";
+    std::cout << "  " << "███" << GREEN << "███" << YELLOW << "███" << BLUE << "███" << MAGENTA << "███" << CYAN << "███" << RESET << "\n\n";
 }
 
 void TF_os_main() {
@@ -33,7 +96,11 @@ void TF_os_main() {
     string dop_comm;
     string shell = "TH-Shell(C++architecture)";
     string namefold;
-    vector<string> folders;
+    vector<string> folders {
+        "root",
+            "hda1/TSD"
+    };
+    fstream File("dates.txt");
 
     while (true) {
         
@@ -67,22 +134,75 @@ void TF_os_main() {
             cout << "  whoa -u      - show current user\n";
             cout << "  whoa -t      - show shell type\n";
             cout << "  exit / quit  - exit T-FOS\n";
+            cout << "     RELEASE 0.0.3 ADDS:\n";
+            cout << "  retyping the folcr function\n";
+            cout << "cds --          - come you in the system tree\n";
+            cout << "cds 'folder'    - come you in the your folder \n";
+            cout << "calc 'n1' 'math' 'n2'  - calculator\n";
+            cout << "pong 'num'      - the sound\n";
         }
 
         else if (command == "folcr") {
-            cin >> namefold;
-            folders.push_back(namefold);
-            cout << "[OK] Folder '" << namefold << "' created.\n";
+            ;
+            ;
+            string folder_name;
+            cin >> folder_name;
+            ;
+            folders.push_back(folder_name);
+            ;
+            foldersesc* newFolder = new foldersesc{folder_name , current};
+            current->children[folder_name] = newFolder;
+            ;
+            ;
+            ;
+
         }
         
         else if (command == "lsc") {
-            cout << "--- Folders list ---\n";
-            if (folders.empty()) {
-                cout << " (no folders created yet)\n";
-            } else {
-                for (auto x : folders) {
-                    cout << " . " << x << endl;
+            cout << " ||folder list|| " << endl;
+            if(current->children.empty()) {
+                for(auto x : folders) {
+                    cout << "__>" << x << endl;
                 }
+            }   else { 
+                for(auto const& [name , folder_ptr] : current->children) {
+                    cout << "-->" << GREEN  << name << RESET << endl;
+                }
+            }
+        }
+        else if(command == "cds") {
+            string target;
+            cin >> target;
+
+            if(target == "--") {
+                if(current->parent != nullptr) {
+                    current = current->parent;
+                    
+                }
+                else {
+                    cout << "TH-Shell::you are in system tree" << endl;
+                }
+                }
+            else if(current->children.count(target)) {
+                current = current->children[target];
+            }
+            else if(target == ".") {
+
+            }
+            else if(target == "-h") {
+                cout << " |-----cds help----\n";
+                cout << " | cds 'folder'    - come your in your folder\n";
+                cout << " | cds --          - come you in the dydtem tree\n";
+                cout << " | cds -h          - see cds functions\n";
+                cout << " |-----------------\n";
+            }
+            else if(target == "hda/TSD" || target == "root") {
+        
+    cout << "[ERROR:]:you are dont the THAdmin, this is system files\n"; 
+            }
+            else {
+                cout << "TH-Shell::incorrect command: '" << target << "' please for help type: cds -h" << endl;
+                cout << "cds::incorrect command: '" << target << "' please for help type: cds -h" << endl;
             }
         }
         
@@ -102,17 +222,49 @@ void TF_os_main() {
             exit(0);
         }
         
+        else if(command == "calc") {
+            calulator();
+
+        }
+        else if(command == "pong") {
+            int numsound;
+            cin >> numsound;
+            if(numsound == 1){
+        int sound1 = system("paplay /usr/share/sounds/freedesktop/stereo/message-new-instant.oga");
+            }
+            else if(numsound == 2){
+                int sound = system("paplay /usr/share/sounds/gnome/default/alarms/crossing-bell.oga");
+            }
+            else {
+                cout << "[ERROR]:Invalid sound number" << endl;
+            }
+        }
+
+        else if(command == "cels") {
+            string name;
+            cin >> name;
+            cout << "===T-FOS cels text redactor" << endl;
+            cels(name);
+        }
+        
         else {
             cout << "[ERROR] Command not found: '" << command << "'. Type -help- for info.\n";
         }
+
+
+        
     }
 }
 
 int main() {
 
 
-    cout << "=== Welcome to T-FOS v0.0.1 (C++ Core) ===\n\n";
+    cout << "=== Welcome to T-FOS v0.0.3 (C++ Core) ===\n\n";
     TF_os_main();
 
     return 0;
 }
+
+
+
+
